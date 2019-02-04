151e37f7892d385de1c39fe7ebf3bd259bafe8b2
Pranav <poudelpranav@gmail.com>
2019-02-04 12:30:38 +0545

    created user database model

--- Server Side/Application/model.py
+++ Server Side/Application/model.py
@@ -1,17 +1,34 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+import os
 from passlib.apps import custom_app_context as pwd_context
+
+#Configuring Applicaton
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///user.sqlite3'
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir,'db.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+#Init Database
 db = SQLAlchemy(app)
+#Init Ma
+ma = Marshmallow(app)
+
+#Database Model
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key = True)
+    firstname = db.Column(db.String(32))
+    lastname = db.Column(db.String(32))
     username = db.Column(db.String(32), index = True)
     password_hash = db.Column(db.String(128))
-    email = db.Column(db.string(32))
-    district = db.relationship("Districts")
-
+    email = db.Column(db.String(32))
+    contactno = db.Column(db.String(10),nullable = False)
+    location_id = db.Column(db.Integer,db.ForeignKey('districts.id'))
+    location = db.relationship('Districts')
+    districtName = db.Column(db.String(15),nullable = False)
     def hash_password(self, password):
         self.password_hash = pwd_context.encrypt(password)
 
@@ -19,4 +36,16 @@ class User(db.Model):
         return pwd_context.verify(password, self.password_hash)
 
 class Districts(db.Model):
-#for database model
+    __tablename__ = 'districts'
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(15),nullable = False)
+
+
+#Marshmallow Serializer
+class UserSchema(ma.Schema):
+    class Meta:
+        fields = ['firstname','lastname','username','email','contactno','districtName']
+
+User_Schema = UserSchema(strict = True)
+
+
