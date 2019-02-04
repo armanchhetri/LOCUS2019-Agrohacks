3c3a8b49f7f4aa8cbe8791a946b7d0000b0cc5a3
Arman <armanchhetri44@gmail.com>
2019-02-04 10:30:55 +0545

    models and app

--- Server Side/Application/app.py
+++ Server Side/Application/app.py
@@ -1,1 +1,50 @@
+from models import User
+from flask_httpauth import HTTPBasicAuth
+
+from flask import Flask, jsonify, make_response,abort,request
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+auth = HTTPBasicAuth()
+
+# from flask_sqlalchemy import SQLAlchemy
+# from passlib.apps import custom_app_context as pwd_context
+#
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///user.sqlite3'
+db = SQLAlchemy(app)
+
+
+@auth.verify_password
+def verify_password(username, password):
+    user = User.query.filter_by(username = username).first()
+    if not user or not user.verify_password(password):
+        return False
+    # g.user = user
+    return True
+
+@app.route("/api/resource",methods=['GET'])
+@auth.login_required
+def resource():
+    return jsonify({"hello":"hello you are logged in!"})
+
+
+@app.route('/api/sign-up', methods = ['POST'])
+def new_user():
+    username = request.json.get('username')
+    password = request.json.get('password')
+    if username is None or password is None:
+        abort(400) # missing arguments
+    if User.query.filter_by(username = username).first() is not None:
+        abort(400) # existing user
+    user = User(username = username)
+    user.hash_password(password)
+    db.session.add(user)
+    db.session.commit()
+    return jsonify({ 'username': user.username }), 201
+
+
+
+if __name__ == '__main__':
+    db.create_all()
+    app.run(debug=True )
 #for application
--- Server Side/Application/model.py
+++ Server Side/Application/model.py
@@ -1,1 +1,22 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from passlib.apps import custom_app_context as pwd_context
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///user.sqlite3'
+db = SQLAlchemy(app)
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(32), index = True)
+    password_hash = db.Column(db.String(128))
+    email = db.Column(db.string(32))
+    district = db.relationship("Districts")
+
+    def hash_password(self, password):
+        self.password_hash = pwd_context.encrypt(password)
+
+    def verify_password(self, password):
+        return pwd_context.verify(password, self.password_hash)
+
+class Districts(db.Model):
 #for database model
